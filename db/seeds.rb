5b99e64ec71ecf9f2d2f124e1a34d34ed08a5861
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-21 16:24:57 +0200

    Add migration for Message table and seed it with dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Message.create(message: 'Hello There')
+Message.create(message: 'Whats up')
+Message.create(message: 'Muli Bwanji')
+Message.create(message: 'Whats poppping')
+Message.create(message: 'Uli Che')
