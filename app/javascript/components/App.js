47922fcdcdc2448d449fcf7a62cb989f8a860b1e
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-30 10:21:43 +0200

    Add store redux provider to the App

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Store from '../redux/store';
+import { Provider } from 'react-redux';
 import Greeting from './Greeting';
 
 class App extends React.Component {
   render() {
     return(
+      <Provider store={Store}>
       <BrowserRouter>
         <div>
           Hello World
@@ -14,6 +17,7 @@ class App extends React.Component {
           <Route path="/main" element={<Greeting />} />
         </Routes>
       </BrowserRouter>
+      </Provider>
     );
   }
 }
