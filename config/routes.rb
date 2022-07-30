8da3b35784e54027edd964f0d2ff0553201d3584
Alick Nyirenda <44676055+Beardless-sheik@users.noreply.github.com>
2022-07-30 09:21:40 +0200

    Change typo in routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :main, only: [:index,:show]
   namespace :api do 
     namespace :v1 do
-      resources :greetings, only: [:index]
+      resources :greeting, only: [:index]
     end
   end
 end
